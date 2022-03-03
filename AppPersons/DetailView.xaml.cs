f64606c348aa7d287515c2c9a4131356e2baa01c
ArturoLunaa <jlg73100@udelasalle.edu.mx>
2022-03-03 13:34:03 -0600

    boton delete

---
+++ AppPersons/DetailView.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPersons
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailView : ContentPage
+    {
+        MainPage MainPage;
+        PersonModel Person = new PersonModel();
+
+        public DetailView(MainPage mainPage)
+        {
+            InitializeComponent();
+
+            this.MainPage = mainPage;
+        }
+
+        public DetailView(MainPage mainPage, PersonModel person)
+        {
+            InitializeComponent();
+
+            this.MainPage = mainPage;
+
+            // Cargar persona seleccionada
+            this.Person.ID = person.ID;
+            ImagePicture.Source = person.Picture;
+            EntryPicture.Text = person.Picture;
+            EntryFirstName.Text = person.FirstName;
+            EntryLastName.Text = person.LastName;
+            EntryAge.Text = person.Age.ToString();
+            EntryPhone.Text = person.Phone;
+        }
+
+        private void ButtonSave_Clicked(object sender, EventArgs e)
+        {
+            if (this.Person.ID > 0)
+            {
+                // Actualizar
+                for (int index = 0; index < this.MainPage.Persons.Count; index++)
+                {
+                    if(this.MainPage.Persons[index].ID == this.Person.ID)
+                    {
+                            this.MainPage.Persons[index].Picture = EntryPicture.Text;
+                            this.MainPage.Persons[index].FirstName = EntryFirstName.Text;
+                            this.MainPage.Persons[index].LastName = EntryLastName.Text;
+                            this.MainPage.Persons[index].Age = int.Parse(EntryAge.Text);
+                            this.MainPage.Persons[index].Phone = EntryPhone.Text;
+                            break;   
+                    }
+                }
+            }
+            else
+            {
+                // Crear
+                Person.Picture = EntryPicture.Text;
+                Person.FirstName = EntryFirstName.Text;
+                Person.LastName = EntryLastName.Text;
+                Person.Age = int.Parse(EntryAge.Text);
+                Person.Phone = EntryPhone.Text;
+
+                // Agregamos la nueva persona a nuestro listado de personas
+                this.MainPage.Persons.Add(Person);
+            }
+            // Refrescamos nuestro collectionviwe
+            this.MainPage.ListRefresh();
+
+            //Regresamos la vista del listado
+            this.Navigation.PopAsync();
+        }
+
+        private void ButtonCancel_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        //Funcion delete
+        private void ButtonDelete_Clicked(object sender, EventArgs e)
+        {
+            if (this.Person.ID > 0)
+            {
+                //Lo mismo que en actualizar, buscamos por el ID la persona pero ahora la eliminamos con RemoveAt
+                for (int index = 0; index < this.MainPage.Persons.Count; index++)
+                {
+                    if (this.MainPage.Persons[index].ID == this.Person.ID)
+                    {
+                        this.MainPage.Persons.RemoveAt(index);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                //Crear
+                Person.Picture = EntryPicture.Text;
+                Person.FirstName = EntryFirstName.Text;
+                Person.LastName = EntryLastName.Text;
+                Person.Age = int.Parse(EntryAge.Text);
+                Person.Phone = EntryPhone.Text;
+
+                // Agregamos la nueva persona a nuestro listado de personas
+                this.MainPage.Persons.Add(Person);
+            }
+            //Refrescamos nuestro collectionview
+            this.MainPage.ListRefresh();
+
+            //Regresamos la vista del listado
+            Navigation.PopAsync();
+        }
+    }
+}
---
+++ AppPersons/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace AppPersons
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<PersonModel> Persons;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ButtonLoadPersons_Clicked(object sender, EventArgs e)
+        {
+            ButtonLoadPersons.IsEnabled = true;
+            IndicatorPersons.IsRunning = false;
+
+            Persons = new List<PersonModel>
+            {
+                new PersonModel
+                {
+                    ID = 1,
+                    FirstName = "Cristiano",
+                    LastName = "Ronaldo",
+                    Age = 37,
+                    Phone = "234242324",
+                    Picture = "https://www.realmadrid.com/img/vertical_380px/cristiano_550x650_20180917025046.jpg"
+                },
+                new PersonModel
+                {
+                    ID = 2,
+                    FirstName = "Leo",
+                    LastName = "Messi",
+                    Age = 35,
+                    Phone = "531243453",
+                    Picture = "https://img.olympicchannel.com/images/image/private/t_1-1_600/f_auto/v1538355600/primary/wfrhxc0kh2vvq77sonki"
+                },
+                new PersonModel
+                {
+                    ID = 3,
+                    FirstName = "Javier",
+                    LastName = "Hernández",
+                    Age = 33,
+                    Phone = "67890478",
+                    Picture = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2dzCRKm7cm-MC-dVDax1XqqT7sDnkGDgkqqrRkZkse_KhzUQUZ3JeDdsNVDiwGGrL_jY&usqp=CAU"
+                }
+            };
+            ListPersons.ItemsSource = Persons;
+
+            IndicatorPersons.IsRunning = false;
+            ButtonLoadPersons.IsEnabled = true;
+        }
+        private void ButtonNewPerson_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DetailView(this));
+        }
+
+        public void ListRefresh()
+        {
+            ListPersons.ItemsSource = null;
+            ListPersons.ItemsSource = Persons;
+        }
+
+        private void ListPersons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PersonModel personSelected = e.CurrentSelection.FirstOrDefault() as PersonModel;
+
+            if(personSelected != null)
+            {
+                Navigation.PushAsync(new DetailView(this, personSelected));
+            }
+        }
+    }
+}
